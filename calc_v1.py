83b16760b0c25c323f9d633813b45f075a8ddcb1
Verners Veidmanis <v.veidmanis@gmail.com>
2021-03-15 23:42:47 +0200

    Calc_v1 updated

--- calc_v1.py
+++ calc_v1.py
@@ -1,23 +1,44 @@
 ### Simple Calculator
 # Can use addition, subtraction, multiplication and division
 
-user_numb1 = float(input("Please enter another number: "))
-user_symb = input("Please pick a maths operation to use:\n\
-    \tFor addition enter '+'\n\
-    \tFor subtraction enter '-'\n\
-    \tFor multiplication enter '*'\n\
-    \tFor division enter '\\': ")
-user_numb2 = float(input("Please enter another number: "))
+def add(x, y):
+    return x+y
 
-if user_symb.strip('\'') == '+':
-    result = user_numb1 + user_numb2
-    print(f"The result of {user_numb1} + {user_numb2} = {result}")
-elif user_symb.strip('\'') == '-':
-    result = user_numb1 - user_numb2
-    print(f"The result of {user_numb1} - {user_numb2} = {result}")
-elif user_symb.strip('\'') == '*':
-    result = user_numb1 * user_numb2
-    print(f"The result of {user_numb1} * {user_numb2} = {result}")
-else:
-    result = user_numb1 / user_numb2
-    print(f"The result of {user_numb1} / {user_numb2} = {result}")
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+while True:
+
+    symb = input("Please pick a maths operation to use:\n\
+                    \tFor addition enter 'add'\n\
+                    \tFor subtraction enter 'subtract'\n\
+                    \tFor multiplication enter 'multiply'\n\
+                    \tFor division enter 'divide':     ")
+
+    ops = ['add', 'subtract', 'multiply', 'divide']
+
+    if symb.lower() in ops:
+        user_numb1 = float(input("Please enter a number: "))
+        user_numb2 = float(input("Please enter another number: "))
+
+        if symb.strip(' \'').lower() == 'add':
+            result = add(user_numb1, user_numb2)
+            print(f"{user_numb1} + {user_numb2} = {result}")
+        elif symb.strip(' \'').lower() == 'subtract':
+            result = subtract(user_numb1, user_numb2)
+            print(f"{user_numb1} - {user_numb2} = {result}")
+        elif symb.strip(' \'').lower() == 'multiply':
+            result = multiply(user_numb1, user_numb2)
+            print(f"{user_numb1} * {user_numb2} = {result}")
+        elif symb.strip(' \'').lower() == 'divide':
+            result = divide(user_numb1, user_numb2)
+            print(f"{user_numb1} / {user_numb2} = {result}")
+        break
+    else:
+        print("Invalid maths operator entered!")
